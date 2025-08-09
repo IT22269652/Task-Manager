c8f075ce5163420d34eba491ab6da17950352b27
Nuradha Dakshina <IT22269652@my.sliit.lk>
2025-08-09 17:20:42 +0530

    add new paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,10 @@ import SignUp from "./pages/Auth/SignUp";
 import Dashboard from "./pages/Admin/Dashboard";
 import ManageTasks from "./pages/Admin/ManageTasks";
 import ManageUsers from "./pages/Admin/ManageUsers";
-
 import UserDashboard from "./pages/User/UserDashboard";
-import MyTasks from "./pages/User/MyTasks";
-import ViewTaskDetails from "./pages/User/ViewTaskDetails";
-
 import PrivateRoute from "./routes/PrivateRoute";
 import UpdateTasks from "./pages/Admin/UpdateTasks";
+import UserProfile from "./pages/User/UserProfile";
 
 const App = () => {
   return (
@@ -32,10 +29,9 @@ const App = () => {
           </Route>
 
           {/* User Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
             <Route path="user/dashboard" element={<UserDashboard />} />
-            <Route path="user/tasks" element={<MyTasks />} />
-            <Route path="user/task-details/:id" element={<ViewTaskDetails />} />
+            <Route path="user/profile" element={<UserProfile />} />
           </Route>
         </Routes>
       </Router>
